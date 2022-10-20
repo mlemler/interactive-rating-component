39ab31aa9817f22a01a0a190d937c4deec62f334
Michael Lemler <mail@mlemler.de>
2022-10-20 13:56:45 +0200

    Get rating from request and put it into html

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
-const circles = document.querySelectorAll('.circle');
+const urlParams = new URLSearchParams(window.location.search);
+const rating = urlParams.get('rating');
 
-for (const circle of circles) {
-    circle.addEventListener('click', event => {
-        event.target.classList.toggle('selected');
-    });
+if (rating) {
+    document.getElementById("rating-result").innerText = rating;
 }
