da5ab6929eac07d3c881c39bb0bafdf956af08cd
Michael Lemler <mail@mlemler.de>
2022-10-21 21:19:07 +0200

    Center thank you page items and texts

--- scripts.js
+++ scripts.js
@@ -2,5 +2,5 @@ const urlParams = new URLSearchParams(window.location.search);
 const rating = urlParams.get('rating');
 
 if (rating) {
-    document.getElementById("rating-result").innerText = rating;
+    document.getElementById("rating-value").innerText = rating;
 }
